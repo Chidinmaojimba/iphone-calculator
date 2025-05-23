2218e88c4cad9e0f983702731589ba94753f781e
David Joy <davidjoy.win@gmail.com>
2025-05-23 11:16:37 +0100

    updated the script.js

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-const buttons = document.querySelectorAll("button")
-console.log(buttons)
+const calculation = document.querySelector(".calc");
+const buttons = document.querySelectorAll(".alpha");
+const ac = document.querySelector(".ac");
+let ac_str = ac.innerText;
+
+let str_val = ""
+
+ac.addEventListener('click', ()=>{
+    let arr_val = str_val.split('')
+    arr_val.pop()
+    str_val = arr_val.join('')
+    calculation.innerText = str_val;
+})
+
+for (const button of buttons) {
+    // console.log(button)
+    button.addEventListener('click', function () {
+        let bttn_val = button.innerHTML;
+        console.log(bttn_val)
+        str_val += bttn_val;
+
+        calculation.innerText = str_val;
+    })
+}
+
