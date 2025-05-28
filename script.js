cae1c7363ffeac78046f091578af3b090ddc626e
David Joy <davidjoy.win@gmail.com>
2025-05-28 03:13:59 +0100

    updated the script for the calculator to immitate the real iphone calculator

--- script.js
+++ script.js
@@ -12,10 +12,21 @@ let str_val = "0"
 total_result.style.display = 'none';
 
 ac.addEventListener('click', () => {
-    let arr_val = str_val.split('')
-    arr_val.pop()
-    str_val = arr_val.join('')
-    if (str_val === '') str_val += '0';
+    if (getComputedStyle(total_result).display !== "none") {
+        // when total_result element display style is not 
+        // equal to none, it shows that total result has been 
+        // calculated so the ac button should reinitialize the 
+        // calculation element and the total_result element
+        // which means that the total_result will no longer be 
+        // visible and the calculation will be 0
+        str_val = '0';
+        total_result.style.display = 'none'
+    } else {
+        let arr_val = str_val.split('')
+        arr_val.pop()
+        str_val = arr_val.join('')
+        if (str_val === '') str_val += '0';
+    }
     calculation.innerText = str_val;
 })
 
@@ -23,13 +34,22 @@ for (const button of buttons) {
     // console.log(button)
     button.addEventListener('click', function () {
         let bttn_val = button.innerHTML;
-        console.log(bttn_val)
+
+        let sign_ended = false;
+        signs.forEach((sign) => {
+            if (str_val.endsWith(sign)) {
+                sign_ended = true;
+            }
+        })
+
         if (
             str_val.length === 1 &&
             str_val[0] === '0' && 
             !signs.includes(bttn_val)) {
             str_val = bttn_val;
-        } else str_val += bttn_val;
+        } else if (!(sign_ended && signs.includes(bttn_val))){
+            str_val += bttn_val;
+        } else {}
 
         calculation.innerText = str_val;
     })
